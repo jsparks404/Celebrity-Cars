3ad838deb22630f19096ce6d8c15cce5c51d8cba
Josh Sparks <sparksj404@gmail.com>
2022-10-07 14:14:50 -0400

    make celebrity class

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Celebrity-Cars
+
+
+celebrities
+
+jay leno
+gordon ramsey
+simon cowell
+jeremy clarkson
+justin bieber
+lewis hamilton
+james hetfield
+rowan atkinson
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Celebrity(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.CharField(max_length=300)
+    dob = models.CharField(max_length=50)
+    job = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
