60d95768ad982a7eee5e68e4f65cf919c1d7473c
Josh Sparks <sparksj404@gmail.com>
2022-10-07 12:10:45 -0400

    database set up, home route setup : ;

--- capstone_project/settings.py
+++ capstone_project/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for capstone_project project.
 
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main_app/'
+    'main_app'
 ]
 
 MIDDLEWARE = [
