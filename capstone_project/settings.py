22cf5876ed076d8c21477edbf8e2f241c142eb7c
Josh Sparks <sparksj404@gmail.com>
2022-10-07 13:58:15 -0400

    base.html and home.html, static folder

--- capstone_project/settings.py
+++ capstone_project/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'main_app/static')]
