2249f909492989447dbcb61962fdf9781f73f0f9
Josh Sparks <sparksj404@gmail.com>
2022-10-10 10:08:16 -0400

    car and engine models

--- main_app/models.py
+++ main_app/models.py
@@ -12,3 +12,28 @@ class Celebrity(models.Model):
 
     class Meta:
         ordering = ['name']
+
+
+class Car(models.Model):
+    year = models.CharField(max_length=100, default='#')
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    trim = models.CharField(max_length=100, default='#')
+    celebrity = models.ForeignKey(Celebrity, on_delete=models.CASCADE, related_names="cars")
+
+    def __str__(self):
+        return self.model
+    
+
+class Engine(models.Model):
+    displacement = models.CharField(max_length=100)
+    induction = models.CharField(max_length=100)
+    configuration = models.CharField(max_length=100)
+    hp = models.CharField(max_length=100)
+    torque = models.CharField(max_length=100)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name="engine")
+
+    def __str__(self):
+        return self.displacement
+    
+    
