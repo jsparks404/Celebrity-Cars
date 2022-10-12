0474546052ede059ec99c80d09ba0c6d726bdcc0
Josh Sparks <sparksj404@gmail.com>
2022-10-12 11:48:08 -0400

    full crud up

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('new/', views.CelebrityCreate.as_view(), name="celebrity_create"),
     path('<int:pk>/', views.CelebrityDetail.as_view(), name="celebrity_detail"),
+    path('<int:pk>/delete/', views.CelebrityDelete.as_view(), name="celebrity_delete"),
+    path('<int:pk>/new/', views.CarCreate.as_view(), name="car_create"),
     path('<int:car_pk>/cars/<int:pk>/', views.CarDetail.as_view(), name="car_detail"),
+    path('cars/<int:pk>/update/', views.CarUpdate.as_view(), name="car_update"),
 ]
+
--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.views.generic import DetailView
 from django.urls import reverse
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 # Create your views here.
 
@@ -39,3 +39,33 @@ class CelebrityCreate(CreateView):
     fields = ['name', 'img', 'dob', 'job']
     template_name = "celebrity_create.html"
     success_url = "/"
+
+
+class CelebrityDelete(DeleteView):
+    model = Celebrity
+    template_name = "celebrity_delete_confirmation.html"
+    success_url = "/"
+
+
+class CarCreate(View):
+    def post(self, request, pk):
+        year = request.POST.get('year')
+        make = request.POST.get('make')
+        model = request.POST.get('model')
+        trim = request.POST.get('trim')
+        displacement = request.POST.get('displacement')
+        induction = request.POST.get('induction')
+        configuration = request.POST.get('configuration')
+        hp = request.POST.get('hp')
+        torque = request.POST.get('torque')
+        img = request.POST.get('img')
+        celebrity = Celebrity.objects.get(pk=pk)
+        Car.objects.create(year=year, make=make, model=model, trim=trim, displacement=displacement, induction=induction, configuration=configuration, hp=hp, torque=torque, img=img, celebrity=celebrity)
+        return redirect('celebrity_detail', pk=pk)
+
+    
+class CarUpdate(UpdateView):
+    model = Car
+    fields = ['year', 'make', 'model', 'trim', 'displacement', 'induction', 'configuration', 'hp', 'torque']
+    template_name = "car_update.html"
+    success_url = '/'
