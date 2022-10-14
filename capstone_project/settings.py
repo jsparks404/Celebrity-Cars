c7fe414b38ebc0187b1acd1554b4ec93d543030d
Josh Sparks <sparksj404@gmail.com>
2022-10-14 10:15:37 -0400

    Config deployment

--- capstone_project/settings.py
+++ capstone_project/settings.py
@@ -129,3 +129,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'main_app/static')]
 
 LOGIN_REDIRECT_URL = '/celebrities/'
 LOGOUT_REDIRECT_URL = '/'
+
+import django_heroku
+django_heroku.settings(locals())
